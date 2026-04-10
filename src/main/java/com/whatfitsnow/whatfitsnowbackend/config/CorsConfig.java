a57f702597489db0ad7f030ade96a51533c3509d
Valeria Sliunkova <valeria.sliunkova.ext@consberry.com>
2026-04-10 13:07:38 +0200

    WFN-11: Adjusted cors config

--- src/main/java/com/whatfitsnow/whatfitsnowbackend/config/CorsConfig.java
+++ src/main/java/com/whatfitsnow/whatfitsnowbackend/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
   @Bean
   public CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration config = new CorsConfiguration();
-    config.setAllowedOrigins(List.of("http://localhost:5173"));
+    config.setAllowedOrigins(List.of("http://localhost:5173", "https://whatfitsnowfrontend.onrender.com"));
     config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
     config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
     config.setAllowCredentials(true);
